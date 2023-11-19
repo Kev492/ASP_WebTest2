16a851caabe5d16bd631b1efd79dc941cfb032fc
Kev492 <kyj12342000@gmail.com>
2023-11-20 01:32:21 +0900

    second commit

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -21,16 +21,6 @@ namespace AspWebTest2.Controllers
             var products = _context.PRODUCT.ToList();
             return View("~/Views/Product/Products.cshtml", products);
         }
-
-        [HttpPost]
-        public IActionResult CalculateTotalAmount(string productId, int quantity)
-        {
-            // Perform the calculation on the server for the specific product
-            var product = _context.PRODUCT.FirstOrDefault(p => p.ProductID == productId);
-            var totalAmount = quantity * product.Price;
-
-            // Return the result to the client
-            return Json(totalAmount);
-        }
+        
     }
 }
